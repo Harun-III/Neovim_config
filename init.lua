d3032396e22b350862d2a879a1a1f0d4b7122211
Harun-III <mehdi999100@gmail.com>
2025-04-15 18:21:47 +0100

    nu rnu

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+-- tabstop - shiftwidth
+vim.opt.tabstop = 4 -- Number of spaces that a <Tab> counts for
+vim.opt.shiftwidth = 4 -- Size of an indent
+
+-- make the cursor as block in insert mode
 vim.opt.guicursor = {
   'n-v-c:block', -- normal/visual/cmd: block
   'i-ci-ve:block', -- insert/cmdline-insert/visual-execute: block (your change)
@@ -6,69 +11,8 @@ vim.opt.guicursor = {
 }
 
 --[[
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>file
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
+  If you experience any errors while trying to install kickstart,
+  run `:checkhealth` for more info.
 --]]
 
 -- Set <space> as the leader key
@@ -87,12 +31,10 @@ vim.g.have_nerd_font = false
 
 -- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.opt.mouse = 'c'
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
@@ -225,6 +167,8 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  'ThePrimeagen/vim-be-good',
+  'mfussenegger/nvim-lint',
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,3 +1,25 @@
+local lint = require 'lint'
+
+lint.linters.cpplint = {
+  command = 'cpplint', -- You installed this via `pip3`
+  args = { '--output=emacs', '-' },
+  stream = 'stdout', -- This captures the output
+  ignore_exitcode = true, -- Ignore non-zero exit codes from cpplint
+}
+
+lint.linters_by_ft = {
+  cpp = { 'cpplint' }, -- Use cpplint for .cpp and .h files
+  c = { 'cpplint' }, -- Use cpplint for .c files too
+}
+
+-- Auto-run linting when saving or editing files
+vim.cmd [[
+  augroup Linting
+    autocmd!
+    autocmd BufWritePost *.cpp,*.h lua require('lint').try_lint()
+  augroup END
+]]
+
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
